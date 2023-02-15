e880c132faf148e604273278f41e71b6ec04b009
James Wall <89587655+Jim-Cognito@users.noreply.github.com>
2023-02-15 21:58:31 +0000

    Vite + Vue + Tailwind 🚀⭐️

---
+++ store/src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>Home</h1>
+    <router-link to="/register">register</router-link>
+    <router-link to="/login">login</router-link>
+</template>
+<script lang="ts">
+export default {
+    name: "Home",
+};
+</script>
---
+++ store/src/views/Login.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>Log In</h1>
+    <router-link to="/register">register</router-link>
+    <router-link to="/">home</router-link>
+</template>
+<script lang="ts">
+export default {
+    name: "Login",
+};
+</script>
