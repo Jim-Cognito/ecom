73a1264127cdeb7c73d7efeb0ce7be48933feb0d
JamesWall-hub <james.sean.wall@gmail.com>
2023-02-16 16:12:19 +0000

    add apollo client to main.ts

--- store/src/main.ts
+++ store/src/main.ts
@@ -1,6 +1,15 @@
 import { createApp } from "vue";
+import { ApolloClient, InMemoryCache } from "@apollo/client/core";
+import { DefaultApolloClient } from "@vue/apollo-composable";
 import App from "./App.vue";
-import "./index.css";
 import router from "./router";
 
-createApp(App).use(router).mount("#app");
+const apolloClient = new ApolloClient({
+    uri: "http://localhost:4000/graphql",
+    cache: new InMemoryCache(),
+});
+
+const app = createApp(App);
+app.provide(DefaultApolloClient, apolloClient);
+app.use(router);
+app.mount("#app");
