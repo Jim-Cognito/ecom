2bf5ea617bac772efae44b640cd15ed7000839e3
JamesWall-hub <james.sean.wall@gmail.com>
2023-02-18 23:03:22 +0000

    add google class, remove emit, add googleLogin mutation to google component

--- store/src/components/GoogleLogin.vue
+++ store/src/components/GoogleLogin.vue
@@ -4,29 +4,69 @@
 
 <script lang="ts">
 import { onMounted } from "vue";
-
+import { useRouter } from "vue-router";
+import { useStore } from "vuex";
+import { useGoogleLoginMutation } from "../api/types/types";
+declare class google {
+    static accounts: {
+        id: {
+            initialize: (options: {
+                client_id: string;
+                callback: (response: { credential: string }) => void;
+            }) => void;
+            prompt: () => void;
+            renderButton: (
+                button: HTMLElement,
+                options: {
+                    theme: string;
+                    size: string;
+                    width: number;
+                    height: number;
+                },
+            ) => void;
+        };
+    };
+}
 export default {
     name: "GoogleLogin",
-    emits: ["response"],
-    setup(props, { emit }) {
+    setup() {
+        const store = useStore();
+        const router = useRouter();
+        const {
+            mutate: googleLogin,
+            onDone,
+            onError,
+        } = useGoogleLoginMutation();
         onMounted(() => {
             // initialize google login button
             google.accounts.id.initialize({
                 client_id:
                     "419283728745-4qal7spd25j03u4spd83k40mmvr19h6e.apps.googleusercontent.com",
                 callback: (response) => {
-                    emit("response", response.credential);
+                    googleLogin({
+                        googleToken: response.credential,
+                    });
                 },
             });
+
+            google.accounts.id.prompt();
             const button = document.getElementById("gm-login");
             // get gm-login element
-            google.accounts.id.renderButton(button, {
+            google.accounts.id.renderButton(button!, {
                 theme: "outline",
                 size: "large",
                 width: 224,
                 height: 36,
             });
-            google.accounts.id.prompt();
+        });
+
+        onError((error) => {
+            console.log(error);
+        });
+
+        onDone((result) => {
+            store.commit("setUser", result!.data!.googleLogin);
+            router.push("/");
         });
     },
 };
