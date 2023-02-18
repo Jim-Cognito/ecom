6aff777ed841f8887e4a8e07f794b252bd323086
JamesWall-hub <james.sean.wall@gmail.com>
2023-02-18 19:08:02 +0000

    git add basic nav bar

--- store/src/App.vue
+++ store/src/App.vue
@@ -1,8 +1,11 @@
 <template>
+    <Nav />
     <router-view />
 </template>
 <script lang="ts">
+import Nav from "./components/Nav.vue";
 export default {
     name: "App",
+    components: { Nav },
 };
 </script>
