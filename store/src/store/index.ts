b51ec6d40d1aeaada152d5e73a9d3e98c9fa6c80
JamesWall-hub <james.sean.wall@gmail.com>
2023-02-19 23:55:02 +0000

    add token state to vuex

--- store/src/store/index.ts
+++ store/src/store/index.ts
@@ -3,10 +3,12 @@ import { createStore } from "vuex";
 export default createStore({
     state: {
         user: null,
+        token: null,
     },
     mutations: {
-        setUser(state, { user, refreshToken }) {
+        setUser(state, { user, token }) {
             state.user = user;
+            state.token = token;
         },
         clearUser(state) {
             state.user = null;
