5a69c41cd0c896f20dbbba2d99588a9c0dda5eb0
JamesWall-hub <james.sean.wall@gmail.com>
2023-02-19 23:54:37 +0000

    setup authlink with callback to set header from vuex

--- store/src/main.ts
+++ store/src/main.ts
@@ -1,39 +1,34 @@
 import { createApp } from "vue";
-import { ApolloClient, InMemoryCache } from "@apollo/client/core";
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client/core";
 import { DefaultApolloClient } from "@vue/apollo-composable";
-import { createUploadLink } from "apollo-upload-client";
-import { WhoAmIDocument } from "./api/types/types";
 import App from "./App.vue";
 import store from "./store";
 import router from "./router";
 import "./index.css";
+import { setContext } from "@apollo/client/link/context";
 
 // HTTP connection to the API
-const httpLink = createUploadLink({
+export const httpLink = new HttpLink({
     uri: "http://localhost:4000/graphql",
 });
 
-const apolloClient = new ApolloClient({
-    link: httpLink,
-    cache: new InMemoryCache(),
+const authLink = setContext((_, { headers }) => {
+    // get the authentication token from local storage if it exists
+    const token = store.state.token;
+    // return the headers to the context so httpLink can read them
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : "",
+        },
+    };
 });
 
-// use apollo client to call whoAmI query, if authorized, set user in store
-apolloClient
-    .query({
-        query: WhoAmIDocument,
-    })
-    .then((result) => {
-        if (result.data.whoAmI) {
-            store.commit("setUser", {
-                user: result.data.whoAmI,
-                token: localStorage.getItem("token"),
-            });
-        }
-    })
-    .catch((error) => {
-        store.commit("clearUser");
-    });
+export const apolloClient = new ApolloClient({
+    cache: new InMemoryCache(),
+    credentials: "include",
+    link: authLink.concat(httpLink),
+});
 
 const app = createApp(App)
     .use(store)
