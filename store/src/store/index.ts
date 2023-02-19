f7f72f004481d0984f02399990a24648df54bdff
JamesWall-hub <james.sean.wall@gmail.com>
2023-02-19 22:43:46 +0000

    remove old auth from vuex store

--- store/src/store/index.ts
+++ store/src/store/index.ts
@@ -1,27 +1,15 @@
 import { createStore } from "vuex";
-import { setContext } from "@apollo/client/link/context";
 
 export default createStore({
     state: {
         user: null,
     },
     mutations: {
-        setUser(state, { user, token }) {
+        setUser(state, { user, refreshToken }) {
             state.user = user;
-            localStorage.setItem("token", token);
-            setContext((_, { headers }) => {
-                // return the headers to the context so httpLink can read them
-                return {
-                    headers: {
-                        ...headers,
-                        authorization: token ? `Bearer ${token}` : "",
-                    },
-                };
-            });
         },
         clearUser(state) {
             state.user = null;
-            localStorage.removeItem("token");
         },
     },
 });
