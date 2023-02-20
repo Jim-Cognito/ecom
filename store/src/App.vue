4af1d866eb8d6bb7ae89b2853e40e951b7f66e1b
JamesWall-hub <james.sean.wall@gmail.com>
2023-02-20 13:05:22 +0000

    fetch refresh token app mounts

--- store/src/App.vue
+++ store/src/App.vue
@@ -1,11 +1,38 @@
 <template>
-    <Nav />
-    <router-view />
+    <div v-if="loading">Loading...</div>
+    <div v-else>
+        <Nav />
+        <router-view />
+    </div>
 </template>
 <script lang="ts">
+import { onMounted, ref } from "vue";
 import Nav from "./components/Nav.vue";
+import axios from "axios";
 export default {
     name: "App",
     components: { Nav },
+    setup() {
+        const loading = ref<boolean>(true);
+
+        onMounted(() => {
+            fetch("http://localhost:4000/refresh_token", {
+                method: "POST",
+                credentials: "include",
+            })
+                .then(async (x) => {
+                    const data = await x;
+                    console.log(data);
+                    loading.value = false;
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        });
+
+        return {
+            loading,
+        };
+    },
 };
 </script>
