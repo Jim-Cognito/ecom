d5693f3e7b05ac58f82787506094f2abffd6a3c6
JamesWall-hub <james.sean.wall@gmail.com>
2023-02-20 13:03:27 +0000

    update cookie attrs

--- graph/src/graphql/auth/refresh-token.ts
+++ graph/src/graphql/auth/refresh-token.ts
@@ -19,5 +19,8 @@ export const createRefreshToken = (userId: number) => {
 export const sendRefreshToken = (res: Response, token: string) => {
     res.cookie("jid", token, {
         httpOnly: true,
+        sameSite: "none",
+        path: "/",
+        secure: true,
     });
 };
