1b7b63099dc5ebfc372dbbe175d8e2ce72827f58
JamesWall-hub <james.sean.wall@gmail.com>
2023-02-21 22:32:23 +0000

    add pay routes

--- store/src/components/Nav.vue
+++ store/src/components/Nav.vue
@@ -5,6 +5,7 @@
         <router-link class="block" to="/register">Register</router-link>
         <router-link class="block" to="/products">Products</router-link>
         <router-link class="block" to="/auth">Auth Test</router-link>
+        <router-link class="block" to="/pay">Pay</router-link>
     </div>
 </template>
 
