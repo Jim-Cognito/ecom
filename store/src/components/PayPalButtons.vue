7d948cef4d643690a4e5adaf28e51de9888a234a
JamesWall-hub <james.sean.wall@gmail.com>
2023-02-21 22:30:49 +0000

    init basic paypal component and pay view

---
+++ store/src/components/PayPalButtons.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="paypal-buttons" class="w-56"></div>
+</template>
+
+<script lang="ts">
+import { loadScript } from "@paypal/paypal-js";
+
+export default {
+    name: "PayPalButtons",
+    setup() {
+        loadScript({
+            "client-id": import.meta.env.VITE_PAYPAL_CLIENT_ID!,
+            components: "buttons",
+        })
+            .then((paypal) => {
+                // start to use the PayPal JS SDK script paypal
+                paypal!.Buttons!({
+                    style: {
+                        label: "pay",
+                    },
+                }).render("#paypal-buttons");
+            })
+            .catch((err) => {
+                console.error("Failed to load the PayPal JS SDK script", err);
+            });
+    },
+};
+</script>
---
+++ store/src/views/Pay.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="flex flex-col items-center mt-36">
+        <PayPalButtons />
+    </div>
+</template>
+
+<script lang="ts">
+import PayPalButtons from "../components/PayPalButtons.vue";
+
+export default {
+    name: "Pay",
+    components: { PayPalButtons },
+};
+</script>
